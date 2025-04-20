c06e99010b9a2e58088abf9bcb7914301dcb83f0
vkatari10 <kaskat2021@gmail.com>
2025-04-20 13:15:34 -0400

    Tested API -- works

--- src/data/alpaca.py
+++ src/data/alpaca.py
@@ -1,1 +1,25 @@
+# Alpaca imports
+from alpaca_trade_api.rest import REST, TimeFrame  # Or use your specific Alpaca version import
 
+# Other imports
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+API_KEY = os.getenv("ALPACA_API_KEY")
+SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
+
+# Initialize the Alpaca client
+trading_client = REST(API_KEY, SECRET_KEY, base_url="https://paper-api.alpaca.markets")
+
+# Get our account information
+account = trading_client.get_account()
+
+# Check if our account is restricted from trading
+if account.trading_blocked:
+    print('Account is currently restricted from trading.')
+
+# Check how much money we can use to open new positions
+print('${} is available as buying power.'.format(account.buying_power))
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+import os
+
+
+API_KEY = os.getenv("ALPACA_API_KEY")
+SECRET_KEY = os.getenv("ALPACA_SECRET_KEY");
