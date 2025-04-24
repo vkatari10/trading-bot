bacdc7d0138683c91f4a19a5f4cf64c8bccfbeed
vkatari10 <kaskat2021@gmail.com>
2025-04-24 11:10:34 -0400

    Renamed directories

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Trading Bot Source Files
+
+This folder contains numerous files for the bot including backtesting strategies, data analysis, wrapper methods for placing orders via the Alpaca API, and other related code needed
---
+++ backend/drivers/main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+import time
+
+from src.alpaca_api.ordering import place_market_order, cancel_all_orders, place_limit_order, cancel_order
+
+
+API_KEY = os.getenv("ALPACA_API_KEY")
+SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
+
+id = place_limit_order("day", "AAPL", "2", "buy", True, "199")
+time.sleep(5)
+cancel_order(id)
