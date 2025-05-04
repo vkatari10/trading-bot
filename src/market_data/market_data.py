0df441caeca9918c8859de59c3bde4ad8e752ce1
vkatari10 <kaskat2021@gmail.com>
2025-05-04 18:14:53 -0400

    Added finnhub comment doc

--- src/market_data/market_data.py
+++ src/market_data/market_data.py
@@ -14,7 +14,6 @@ Date: 04/25/2025
 import finnhub
 import os
 from dotenv import load_dotenv
-import requests
 from typing import Dict, Any
 
 load_dotenv()
@@ -24,10 +23,17 @@ SECRET_KEY = os.getenv("FINNHUB_SECRET_API_KEY")
 
 finnhub_client = finnhub.Client(api_key=API_KEY)
 
-def get_quote(ticker: str) -> Dict[str, any]:
-    '''
 
+def get_quote(ticker: str) -> Dict[str, Any]:
+    '''
+    Returns the current open, high, low prices for a given ticker
+
+    Arguments:
+
+    ticker: the ticker of the asset
+
+    Returns:
+
+    return a JSON object giving the high, open, low, close prices of the asset
     '''
     return finnhub_client.quote(ticker)
-
-print(get_quote("AAPL"))
