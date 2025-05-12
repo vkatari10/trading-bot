81b5a682d59e13b64e6da909bf73c11eface65a1
vkatari10 <kaskat2021@gmail.com>
2025-05-12 14:50:05 -0400

    Added primitive trading strategy

--- src/backtesting/backtesting.py
+++ src/backtesting/backtesting.py
@@ -31,7 +31,66 @@ class SmaCross(bt.Strategy):
 
 
 class CustomStrategy(bt.Strategy):
-    pass
+    '''
+    SMAs -> slow cross fast
+    EMAs -> 9, 12, 26 for MACD
+    Bollinger Bands -> price cross above or below
+    '''
+    params = dict(sma_slow=30, sma_fast=10, macd1=12, macd2=26,
+                  macdsig=9)
+
+
+    def __init__(self):
+        self.sma_slow = bt.ind.SMA(period=self.p.sma_slow)
+        self.sma_fast = bt.ind.SMA(period=self.p.sma_fast)
+        self.macd = bt.ind.MACD(self.data,
+                                period_me1 = self.p.macd1,
+                                period_me2 = self.p.macd2,
+                                period_signal = self.p.macdsig)
+        self.bands = bt.ind.BollingerBands()
+        self.crossover = bt.ind.CrossOver(self.sma_slow, self.sma_fast)
+        self.macd_cross = bt.ind.CrossOver(self.macd.macd, self.macd.signal)
+        self.rsi = bt.ind.RSI()
+
+    def next(self):
+
+
+        if len(self) < max(self.p.sma_slow, 26): # 26 for MACD
+            return
+
+        signal = 0
+
+        # Determine SMA Signals
+        if self.crossover > 0:
+            signal += 1
+        elif self.crossover < 0:
+            signal -= 1
+
+        # Determine MACD Signal Line Cross
+        if self.macd_cross > 0:
+            signal += 1
+        elif self.macd_cross < 0:
+            signal -= 1
+
+        # RSI
+        if self.rsi < 30:
+            signal += 1
+        elif self.rsi > 70:
+            signal -= 1
+
+        # Bollinger Bands
+        if self.data < self.bands.lines.bot:
+            signal += 1
+        elif self.data > self.bands.lines.top:
+            signal -= 1
+
+        if signal > 0:
+            #if not self.position:
+                self.buy(size=100)
+        elif signal < 0:
+            if self.position:
+                self.close(size=100)
+
 
 # Download data
 df = yf.download("MSFT", start="2011-01-01", end="2012-12-31")
@@ -44,7 +103,7 @@ df.columns = df.columns.get_level_values(0)
 data = bt.feeds.PandasData(dataname=df)
 
 cerebro = bt.Cerebro()
-cerebro.addstrategy(SmaCross)
+cerebro.addstrategy(CustomStrategy)
 cerebro.adddata(data)
 
 cerebro.run()
