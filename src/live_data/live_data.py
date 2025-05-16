7c7efc67fcded053a6e0af63a1979fc76a2e43fb
vkatari10 <kaskat2021@gmail.com>
2025-05-16 15:56:20 -0400

    Added pybind modules and test file

--- src/live_data/live_data.py
+++ src/live_data/live_data.py
@@ -10,29 +10,62 @@ Modules Used
 Author: Vikas Katari
 Date: 05/13/25
 '''
-
-'''
-export the dataframe as a CSV, we can then load it into here
-and then keep appending the new data
-
-take the dataframe, take the close and convert it to a series
-
--> take the series
--> append the new data from finnhub
--> recompute using finta
--> get new techicals
--> reappend back to the original dataframe
--> make model predict on the last added row if buy/sell/hold
-
-Execution:
--> bring the dataframe into the driver file
--> get quote update from finnhub
-->
-'''
 import pandas as pd
 import numpy as np
 from finta import TA
 import src.data.technicals as te
+from typing import List
+import datetime
+
+import example
+
+example.print()
+
+
+def initialize_row(df: pd.DataFrame, current: float, high: float,
+                   low: float, _open:float) -> pd.DataFrame:
+    '''
+    Insert a new row in to the dataframe with the current, high,
+    low and open values to recompute technical indicators with
+    the newest updated price
+
+    Args:
+
+    df (pd.DataFrame): dataframe that contains a currrent, high
+    low, _open values from index 0-3 inclusive, default from
+    the yfinance API
+    current (float): the current price column
+    high (float): the high price column
+    low (float): the low price column
+    _open (float): the open price column
+
+    ReturnL
+
+    A dataframe contianing the current, high, low, _open
+    values and technical indicators. Sets all others
+    values in all other columns to 0.
+    '''
+    cols_list = df.columns
+
+    init_data = dict()
+
+    # intialize new row
+    init_data[cols_list[0]] = [current]
+    init_data[cols_list[1]] = [high]
+    init_data[cols_list[2]] = [low]
+    init_data[cols_list[3]] = [_open]
+
+    for i in range(5, len(cols_list)):
+        init_data[cols_list[i]] = [float(0.0)]
+
+
+    new_row = pd.DataFrame(init_data)
+    new_row.index = datetime.datetime.now()
+
+    _new = pd.concat([df, new_row], axis=0)
+
+    return _new
+
 
 def append_new(df: pd.DataFrame, current: float,
                high: float, low: float, _open: float) -> pd.DataFrame:
@@ -40,40 +73,28 @@ def append_new(df: pd.DataFrame, current: float,
     Appends new price to the 'close' pd.Series obtained from
     get_close() along with the recomputated technicals
     '''
-    new_row = pd.DataFrame({
-        'close': [current],
-        'high': [high],
-        'low': [low],
-        'open': [_open],
-        'RSI': [0],
-        'BB_UPPER': [0],
-        'BB_LOWER': [0],
-        'MACD': [0],
-        'SIGNAL': [0],
-        'SMA(10)': [0],
-        'SMA(30)': [0],
-        'ACTION': [0]
-    })
 
-    new_row.index = [len(df)]
+    # insert bottom row into DF
+    _new = initialize_row(df, current, high, low, _open)
 
-    _new = pd.concat([df, new_row], axis=0)
+    # recompute techncials for the last rows
+    rsi = TA.RSI(_new)
+    bbands = TA.BBANDS(_new)
+    macd = TA.MACD(_new)
+    sma30 = te.sma(_new, days=30, col='close')
+    sma10 = te.sma(_new, days=10, col='close')
 
-    rsi = TA.RSI(df)
-    bbands = TA.BBANDS(df)
-    macd = TA.MACD(df)
-    sma30 = te.sma(df, days=30, col='close')
-    sma10 = te.sma(df, days=10, col='close')
+    # manually inesrt the technical recoputation into the last row
+    _new.iat[-1, 5] = rsi.iat[-1]
+    _new.iat[-1, 6] = bbands['BB_LOWER'].iat[-1]
+    _new.iat[-1, 7] = bbands['BB_UPPER'].iat[-1]
+    _new.iat[-1, 8] = macd['MACD'].iat[-1]
+    _new.iat[-1, 9] = macd['SIGNAL'].iat[-1]
+    _new.iat[-1, 10] = sma10.iat[-1, 0]
+    _new.iat[-1, 11] = sma30.iat[-1, 0]
 
 
-    _new.iat[-1, 4] = rsi.iat[-1]
-    _new.iat[-1, 5] = bbands['BB_LOWER'].iat[-1]
-    _new.iat[-1, 6] = bbands['BB_UPPER'].iat[-1]
-    _new.iat[-1, 7] = macd['MACD'].iat[-1]
-    _new.iat[-1, 8] = macd['SIGNAL'].iat[-1]
-    _new.iat[-1, 9] = sma10.iat[-1, 0]
-    _new.iat[-1, 10] = sma30.iat[-1, 0]
-    _new.iat[-1, 11] = 0
+    _new.iat[-1, -1] = 0 # Leave 0 to let ML predict action
 
     return _new
 
