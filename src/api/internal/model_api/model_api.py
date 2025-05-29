6e95191d20f24f21ace15c38c126876d49bb02c0
vkatari10 <kaskat2021@gmail.com>
2025-05-29 17:14:01 -0400

    feat(src): Added skeleton code for ML API

--- src/api/internal/model_api/model_api.py
+++ src/api/internal/model_api/model_api.py
@@ -14,42 +14,38 @@ Date: 05/28/2025
 from flask import Flask
 import pickle
 import json
-
-"""
-Assume input jsons in src/logic look like the example
-
-from go we get -> [SMA_10: 200, SMA_20: 205]
-
-we need to fit this to model and let it predict something
-and then push that back out for go to grab
-"""
+import numpy as np
 
 # Load in deciding ML model
 with open('src/ml/models/decider/model.pkl', 'rb') as f:
     model = pickle.load(f)
+
+with open('src/logic/features.json') as f:
+    features = json.load(f)
 
 
 app = Flask(__name__)
 # Add find free socket method here
 
 
-@app.route('/predict')
+@app.route('api/ml-hotline', methods=['GET', 'POST'])
 def send_prediction():
-    pass
-    # import model
-    # get data from the get_lastest data method
-    # make prediction
-    # send prediction back as JSON
+    '''
+    The runtime engine (in this case Go) will Post an array as JSON
+    to this endpoint and in this file we will use this array to
+    use the ML model to predict with and then from this side
+    Post again back to the same endpoint the prediction where
+    Go can GET the new prediction and determine further action
+    '''
+
+    features  = get_new_features()
+    features_np = np.array(features)
+
+    prediction = model.predict(features_np)
+
+    return prediction
 
 
-@app.route('/data')
-def get_lastest_data():
-    pass
-    # Go should send this back as a JSON
-    # after getting the lastest market data and recomputing the
-    # technicals we let GO return the data back as an array
-    # that we can then feed to the ML models in the above
-    # method to get a prediction and send back to Go
 
 
 # Run API server
