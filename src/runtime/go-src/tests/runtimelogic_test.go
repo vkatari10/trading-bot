3f659a12fc23fda4f505f797a885258e6f1114f3
vkatari10 <kaskat2021@gmail.com>
2025-06-19 15:44:30 -0400

    Added Updater Tests for Technical in go-src

--- src/runtime/go-src/tests/runtimelogic_test.go
+++ src/runtime/go-src/tests/runtimelogic_test.go
@@ -18,10 +18,7 @@ func TestLoadBurnData(t *testing.T) {
 		t.Errorf("TestLoadBurnData failed with error %v", err)
 	} // if
 
-	testBurnData := []float64{
-	82.13, 57.84, 96.47, 51.29, 88.11, 74.25, 59.63, 92.05, 68.74, 85.97,
-    60.22, 78.49, 53.38, 90.16, 71.02, 66.44, 55.87, 83.33, 72.58, 99.44,
-	56.56}
+	testBurnData := getTestData()
 
 	err = engine.LoadBurnData(&userData, testBurnData)
 	if err != nil {
@@ -32,7 +29,6 @@ func TestLoadBurnData(t *testing.T) {
 
 	expected = append(expected, 73.0919) // SMA
 	expected = append(expected, 72.7296) // EMA
-
 
 	for i, ind := range userData.Objects {	
 		switch v := ind.(type) {
@@ -47,10 +43,50 @@ func TestLoadBurnData(t *testing.T) {
 				t.Errorf("TestLoadBurnData fialed for object index %d for type %s (%v)", i, "EMA", err)
 			} // if
 		} // swtich
-
 	} // for
 
 } // TestLoadBurnData
+
+// TestUpdateTechnicals tests if the updating methods for 
+// updating technical indicators works as intended
+func TestUpdateTechnicals(t *testing.T) {
+
+	userData, err := engine.InitUserLogic("testdata.json")
+	if err != nil {
+		t.Errorf("TestUpdateTechnicals failed with error %v", err)
+	} // if
+	
+	testBurnData := getTestData()
+
+	err = engine.LoadBurnData(&userData, testBurnData)
+	if err != nil {
+		t.Errorf("TestUpdateTechnicals failed with error %v", err)
+	} // if
+
+	engine.UpdateTechnicals(&userData, 56.56)
+
+	wants := make([]float64, 0)
+
+	wants = append(wants, 72.20147) // SMA
+	wants = append(wants, 71.13957) // EMA
+
+	for i, ind := range userData.Objects {
+		switch v := ind.(type) {
+		case *engine.EMA:
+			err := checkEquivalent(wants[i], v.Data[len(v.Data) - 1])
+			if err != nil {
+				t.Errorf("TestUpdateTechnicals failed with %v", err)
+			} // if
+		case *engine.SMA:
+			err := checkEquivalent(wants[i], v.Data[len(v.Data) - 1])
+			if err != nil {
+				t.Errorf("TestUpdateTechnicals failed with %v", err)
+			} // if
+		} // switch 
+	} // for
+
+} // TestUpdateTechnicals
+
 
 // checkEquivalent is a helper method that helps
 // determine if 
@@ -65,3 +101,10 @@ func checkEquivalent(want float64, got float64) error {
 func floatsEqual(a, b, epsilon float64) bool {
     return math.Abs(a-b) <= epsilon
 } // floatsEqual
+
+func getTestData() []float64  {
+	return []float64{
+	82.13, 57.84, 96.47, 51.29, 88.11, 74.25, 59.63, 92.05, 68.74, 85.97,
+    60.22, 78.49, 53.38, 90.16, 71.02, 66.44, 55.87, 83.33, 72.58, 99.44,
+	56.56}
+} // getTestData()
