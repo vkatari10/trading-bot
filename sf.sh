32832365eca9f1ce71820413877de944669c4070
vkatari10 <kaskat2021@gmail.com>
2025-06-27 11:04:56 -0400

    Replace C based CLI with shell script

---
+++ sf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function run() {
+    local script="./scripts/$1.sh"
+    if [[ -x "$script" ]]; then
+        "$script"
+    else
+        echo "Command '$1' not found or not executable."
+    fi
+}
+
+while :; do
+    printf "stratforge> "
+    read input
+    [[ "$input" == "exit" ]] && break
+    run "$input"
+done
